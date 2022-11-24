df6765637ff67bbb3752be2d5797e3083461223a
Your Name <you@example.com>
2022-11-24 05:30:52 +0000

    feat: dataload

--- boost/dataloader.py
+++ boost/dataloader.py
@@ -1,4 +1,26 @@
 #opt/ml/data에서 불러오기
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
-def get_data(cfg):
-    pass
+
+
+# 데이터 로드 함수(train, test) from directory
+def get_data(args):
+    train_data = pd.read_csv(os.path.join(args.data_dir, f'FE{args.fe_num}', 'train_data.csv'))
+    test_data = pd.read_csv(os.path.join(args.data_dir, f'FE{args.fe_num}', 'test_data.csv'))
+    return train_data, test_data
+
+
+# 데이터 스플릿 함수
+def data_split(train_data):
+    X = train_data.drop(['answerCode'], axis=1)
+    y = train_data['answerCode']
+
+    X_train, X_valid, y_train, y_valid = train_test_split(
+        train_data.drop(['answerCode'], axis=1),
+        train_data.answerCode,
+        test_size=0.3, # 일단 이 정도로 학습해서 추이 확인
+        shuffle=True,
+    )
+    return X_train, X_valid, y_train, y_valid
--- boost/main.py
+++ boost/main.py
@@ -1,6 +1,8 @@
-
-from dataloader import load_data
+import argparse
+from dataloader import get_data, data_split
 from models import get_model
+from utils import setSeeds
+
 
 # import hydra
 # from omegaconf import DictConfig
@@ -8,12 +10,38 @@ from models import get_model
 # https://towardsdatascience.com/complete-tutorial-on-how-to-use-hydra-in-machine-learning-projects-1c00efcc5b9b
 
 
-def main(cfg: DictConfig):
-    
+def main(args):
+
+    setSeeds(args.seed)
+
+    train_data, test_data = get_data(args)
+    X_train, X_valid, y_train, y_valid = data_split(train_data)
+
     model = get_model(cfg)
     predicts = model(test_data)
 
     # SAVE
 
 if __name__ == '__main__':
-    main()
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--data_dir",
+        default="/opt/ml/data/",
+        type=str,
+        help="data directory",
+    )
+    parser.add_argument(
+        "--fe_num",
+        default='00',
+        type=str,
+        help='feature engineering data file path (ex) 00'
+    )
+    parser.add_argument("--model", default="cat", type=str, help="model type")
+    parser.add_argument("--n_epochs", default=100, type=int, help="number of epochs")
+    parser.add_argument("--lr", default=0.01, type=float, help="learning rate")
+
+    args = parser.parse_args()
+
+    main(args)
